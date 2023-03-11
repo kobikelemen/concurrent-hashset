591a7ce1cfd011def70425146e16e437fe203ac7
kobikelemen <kobi.kelemen20@gmail.com>
2023-03-11 19:07:24 +0000

    non concurrent hash set done (hash_set_sequential.h)

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -4,38 +4,88 @@
 #include <cassert>
 #include <vector>
 #include <functional>
+#include <iostream>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetSequential : public HashSetBase<T> {
  public:
-  explicit HashSetSequential(size_t /*initial_capacity*/) {}
+  explicit HashSetSequential(size_t initial_capacity) {
+    for (size_t i = 0; i < initial_capacity; i++) 
+        table_.push_back(std::vector<T>());
+    size_ = 0;
+    len_ = initial_capacity;
+  }
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
+  bool Add(T elem) final {
+    table_[std::hash<T>()(elem) % len_].push_back(elem);
+    size_ ++;
+    if (Policy()) 
+      Resize();
     return false;
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
+  bool Remove(T elem) final {
+    auto *bucket = &table_[std::hash<T>()(elem) % len_];
+    for (size_t i = 0; i < bucket->size(); i++) {
+      if ((*bucket)[i] == elem) {
+        std::cout << "removed: " << *(bucket->begin() + static_cast<long>(i)) << std::endl;
+        bucket->erase(bucket->begin() + static_cast<long>(i));
+
+        size_ --;
+        return true;
+      }
+    }
     return false;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
+  [[nodiscard]] bool Contains(T elem) final {
+    auto bucket = table_[std::hash<T>()(elem) % len_];
+    for (T e : bucket) {
+      if (e == elem) 
+        return true;
+    }
     return false;
   }
 
   [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+    return size_;
+  }
+
+  void PrintAll() {
+    for (auto bucket : table_) {
+      for (T e : bucket) {
+        std::cout << "  " << e;
+      }
+    }
+    std::cout << std::endl;
   }
 
 private:
   std::vector<std::vector<T>> table_;
   size_t size_; /* Number of elements. */
-  size_t len_;
+  size_t len_; /* Number of buckets (length of table_). */
+
+  bool Policy() {
+    return size_ / len_ > 4;
+  }
+
+  void Resize() {
+    len_ = len_ * 2;
+    std::vector<std::vector<T>> old_table = table_;
+    table_.clear();
+    table_.resize(len_);
+    for (auto bucket : old_table) {
+      for (T elem : bucket) {
+        Add(elem);
+      }
+    }
+  }
+
+  
+
+
 };
 
 #endif  // HASH_SET_SEQUENTIAL_H
--- src/playground.cc
+++ src/playground.cc
@@ -1,1 +1,25 @@
-int main(int /*argc*/, char** /*argv*/) { return 0; }
+#include <iostream>
+
+#include "src/hash_set_sequential.h"
+
+int main(int /*argc*/, char** /*argv*/) {
+    HashSetSequential<int> hash_set = HashSetSequential<int>(10);
+    hash_set.Add(5);
+    hash_set.Add(1);
+    hash_set.Add(4);
+    hash_set.Add(2);
+    hash_set.Add(7);
+    hash_set.Add(11);
+    hash_set.Add(21);
+    hash_set.Add(23);
+    hash_set.Add(13);
+    hash_set.Add(15);
+    hash_set.Add(14);
+    hash_set.Add(33);
+    hash_set.Add(55);
+
+    hash_set.Remove(15);
+    
+    std::cout << hash_set.Contains(15) << std::endl << hash_set.Contains(1) << std::endl << hash_set.Contains(69) << std::endl << hash_set.Size() << std::endl;
+
+}
