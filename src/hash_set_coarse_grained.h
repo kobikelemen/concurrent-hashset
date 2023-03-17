da564a880349ab600e7e0e340ce88fda52c55020
kobikelemen <kobi.kelemen20@gmail.com>
2023-03-17 15:55:46 +0000

    threadSan found a data race after making len_ and size_ atomic

--- src/hash_set_coarse_grained.h
+++ src/hash_set_coarse_grained.h
@@ -16,16 +16,17 @@ class HashSetCoarseGrained : public HashSetBase<T> {
     std::scoped_lock<std::mutex> lock(mutex_);
     for (size_t i = 0; i < initial_capacity; i++) 
         table_.push_back(std::vector<T>());
-    size_ = 0;
-    len_ = initial_capacity;
+    size_.store(0);// = 0;
+    len_.store(initial_capacity);// = initial_capacity;
   }
 
   bool Add(T elem) final {
+    
     if (Contains(elem))
       return true;
     std::scoped_lock<std::mutex> lock(mutex_);
-    table_[std::hash<T>()(elem) % len_].push_back(elem);
-    size_ ++;
+    table_[std::hash<T>()(elem) % len_.load()].push_back(elem);
+    size_.fetch_add(1);// ++;
     if (Policy()) 
       Resize();
     return false;
@@ -33,11 +34,11 @@ class HashSetCoarseGrained : public HashSetBase<T> {
 
   bool Remove(T elem) final {
     std::scoped_lock<std::mutex> lock(mutex_);
-    size_t index = std::hash<T>()(elem) % len_;
+    size_t index = std::hash<T>()(elem) % len_.load();
     for (size_t i = 0; i < table_[index].size(); i++) {
       if (table_[index][i] == elem) {
         table_[index].erase(table_[index].begin() + static_cast<long>(i));
-        size_ --;
+        size_.fetch_sub(1);// --;
         return true;
       }
     }
@@ -45,8 +46,8 @@ class HashSetCoarseGrained : public HashSetBase<T> {
   }
 
   [[nodiscard]] bool Contains(T elem) final {
-    std::scoped_lock<std::mutex> lock(mutex_);
-    auto bucket = table_[std::hash<T>()(elem) % len_];
+    // std::scoped_lock<std::mutex> lock(mutex_);
+    auto bucket = table_[std::hash<T>()(elem) % len_.load()];
     for (T e : bucket) {
       if (e == elem) 
         return true;
@@ -56,7 +57,7 @@ class HashSetCoarseGrained : public HashSetBase<T> {
 
   [[nodiscard]] size_t Size() final {
     std::scoped_lock<std::mutex> lock(mutex_);
-    return size_;
+    return size_.load();
   }
 
   void PrintAll() {
@@ -70,24 +71,24 @@ class HashSetCoarseGrained : public HashSetBase<T> {
 private:
   std::mutex mutex_;
   std::vector<std::vector<T>> table_;
-  size_t size_; /* Number of elements. */
-  size_t len_; /* Number of buckets (length of table_). */
+  std::atomic<size_t> size_; /* Number of elements. */
+  std::atomic<size_t> len_; /* Number of buckets (length of table_). */
 
   bool Policy() {
-    return size_ / len_ > 4;
+    return (size_.load()*1.0) / (len_.load()*1.0) > 4;
   }
 
   void Resize() {
-    size_t old_len = len_;
-    if (old_len != len_) 
+    size_t old_len = len_.load();
+    if (old_len != len_.load()) 
       return;
-    len_ = len_ * 2;
+    len_.store(len_.load() * 2);
     std::vector<std::vector<T>> old_table = table_;
     table_.clear();
-    table_.resize(len_);
+    table_.resize(len_.load());
     for (auto bucket : old_table) {
       for (T elem : bucket) {
-        table_[std::hash<T>()(elem) % len_].push_back(elem);
+        table_[std::hash<T>()(elem) % len_.load()].push_back(elem);
       }
     }
   }
